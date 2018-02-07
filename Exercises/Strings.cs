8e2464b73660fb4da46cff6332c4aac01e4aad59
Simone Saviolo <simone.saviolo+git@gmail.com>
2018-02-07 17:32:04 +0100

    Primi test per stringhe

---
+++ Exercises/Strings.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercises
+{
+    public class Strings
+    {
+		public enum CapitalizationModes
+		{
+			FirstLetterOnlyUppercase,
+			FirstOfEachWordUppercase,
+			AllUppercase,
+			AllLowercase, 
+			Reverse
+		}
+
+		public enum PeopleNameFormats
+		{
+			PlainNameFirst, 
+			PlainSurnameFirst, 
+			HighlightNameFirst, 
+			HighlightSurnameFirst
+		}
+    }
+}
---
+++ Test/TestStrings.cs
@@ -0,0 +1,46 @@
+﻿using Exercises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+	[TestClass]
+	public class TestStrings
+	{
+		[TestMethod]
+		public void Concatenate()
+		{
+			Strings s = new Strings();
+
+			string s1 = "Prima parte";
+			string s2 = "Seconda parte";
+			string name = "Nome";
+			string surname = "Cognome";
+
+			Assert.AreEqual("Prima parte", s.Capitalize(s1, Strings.CapitalizationModes.FirstLetterOnlyUppercase));
+			Assert.AreEqual("Prima Parte", s.Capitalize(s1, Strings.CapitalizationModes.FirstOfEachWordUppercase));
+			Assert.AreEqual("PRIMA PARTE", s.Capitalize(s1, Strings.CapitalizationModes.AllUppercase));
+			Assert.AreEqual("prima parte", s.Capitalize(s1, Strings.CapitalizationModes.AllLowercase));
+			Assert.AreEqual("pRIMA PARTE", s.Capitalize(s1, Strings.CapitalizationModes.Reverse));
+
+			Assert.AreEqual("Nome Cognome", s.FormatName(name, surname));
+			Assert.AreEqual("Nome Cognome", s.FormatName(name, surname, Strings.PeopleNameFormats.PlainNameFirst));
+			Assert.AreEqual("Cognome, Nome", s.FormatName(name, surname, Strings.PeopleNameFormats.PlainSurnameFirst));
+			Assert.AreEqual("Nome COGNOME", s.FormatName(name, surname, Strings.PeopleNameFormats.HighlightNameFirst));
+			Assert.AreEqual("COGNOME, Nome", s.FormatName(name, surname, Strings.PeopleNameFormats.HighlightSurnameFirst));
+
+			Assert.AreEqual("Nome Cognome", s.FormatName(name, surname, false, true));
+			Assert.AreEqual("Cognome, Nome", s.FormatName(name, surname, false, false));
+			Assert.AreEqual("Nome COGNOME", s.FormatName(name, surname, true, true));
+			Assert.AreEqual("COGNOME, Nome", s.FormatName(name, surname, true, false));
+
+			Assert.AreEqual("Prima parteSeconda parte", s.Concatenate(s1, s2));
+			Assert.AreEqual("Prima parteSeconda parte", s.Concatenate(s1, "", s2));
+			Assert.AreEqual("Prima parte, Seconda parte: un'opera di Nome", s.Concatenate(s1, ", ", s2, ": un'opera di ", name));
+
+			Assert.AreEqual("Prima parteSeconda parte", s.Concatenate("", s1, s2));
+			Assert.AreEqual("Prima parte Seconda parte", s.Concatenate(" ", s1, s2));
+			Assert.AreEqual("Prima parte,Seconda parte", s.Concatenate(",", s1, s2));
+			Assert.AreEqual("Prima parte e Seconda parte", s.Concatenate(" e ", s1, s2));
+		}
+	}
+}
