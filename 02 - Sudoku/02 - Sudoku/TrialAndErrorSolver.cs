91044c0e61d1d0eb49febf5f3fca5404b2665182
savissimo <simone.saviolo@gmail.com>
2019-08-08 23:34:07 +0200

    [02] Definito solutore T&E e testato solutore T&E

---
+++ 02 - Sudoku/02 - Sudoku/TrialAndErrorSolver.cs
@@ -0,0 +1,20 @@
+﻿namespace _02___Sudoku
+{
+	public class TrialAndErrorSolver
+	{
+		public void Solve(Sudoku io_sudoku)
+		{
+			// Modifica il sudoku sul posto: non fare copie, modifica direttamente io_sudoku. 
+			// Per leggere una cella: 
+			//	io_sudoku[riga, colonna].Value;
+			// Per modificare una cella: 
+			//	io_sudoku[riga, colonna].Value = <valore>;
+			// Per controllare se il sudoku è risolto
+			//	io_sudoku.IsSolved;
+
+			// Il numero di variabili della tua soluzione (N) non sarà mai 81, ma sarà il numero delle celle *libere* all'inizio. 
+
+			// Non cercare di mettere intelligenza in questo solutore! Un solutore trial and error è stupido per definizione. 
+		}
+	}
+}
---
+++ 02 - Sudoku/02 - Test/SudokuSolverTest.cs
@@ -0,0 +1,18 @@
+﻿using _02___Sudoku;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _02___Test
+{
+	[TestClass]
+	public class SudokuSolverTest
+	{
+		[TestMethod]
+		public void SolveWithTrialAndError()
+		{
+			Sudoku sudoku = new Sudoku("-2--18--43-6--------8--9-6-76---5--2---------1--2---53-7-4--3--------5-69--52--4-");
+			TrialAndErrorSolver solver = new TrialAndErrorSolver();
+			solver.Solve(sudoku);
+			Assert.IsTrue(sudoku.IsSolved);
+		}
+	}
+}
